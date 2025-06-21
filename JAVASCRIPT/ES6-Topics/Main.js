b0fae4dea22486136ac00dbfde6aa3dc33e62d05
leelavathikm1 <leelavathikm1@gmail.com>
2025-06-21 18:01:13 +0530

    Es6

---
+++ JAVASCRIPT/ES6-Topics/Main.js
@@ -0,0 +1,17 @@
+// Logical Operator
+function dispName(name) {
+  return name;
+}
+let a = true;
+let b = false;
+
+console.log(a && dispName("Pavan"));
+console.log(a || dispName("Pavan"));
+
+// Template Literals
+
+let name1 = "Pavan";
+let name2 = "Kumar";
+console.log(name1 + " " + name2, `${name1} `);
+
+//Ternary Operator
