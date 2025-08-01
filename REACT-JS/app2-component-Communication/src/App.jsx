346c60bef9477d5374235227232bd8748b5b3da4
leelavathikm1 <leelavathikm1@gmail.com>
2025-08-01 17:12:52 +0530

    ReactForm

--- REACT-JS/app2-component-Communication/src/App.jsx
+++ REACT-JS/app2-component-Communication/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ParentComp from "./Component-Communication/ParentComp";
+import StudentComp from "./Component-Communication/StudentComp";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div>
-        <ParentComp />
+        {/* <ParentComp /> */}
+        <StudentComp />
       </div>
     </>
   );
--- REACT-JS/app2-component-Communication/src/Component-Communication/ParentComp.jsx
+++ REACT-JS/app2-component-Communication/src/Component-Communication/ParentComp.jsx
@@ -2,29 +2,33 @@ import { Component } from "react";
 import ChildComp from "./ChildComp";
 
 export default class ParentComp extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     // This is called as state in parent
     this.state = {
       message: "Parent Component text Is Called from Child Component !!",
+      showPicture: true,
     };
   }
-
-  //   changeMsgInParent = () => {
-  //     this.setState({
-  //       newMessage: "Text Changed in Parent When You clicked the Button",
-  //     });
-  //   };
+  // If you want to change any value inside the state, you should call this.setState Method
+  changeMsgInParent = () => {
+    this.setState({
+      message: "Text Changed in Parent When You clicked the Button",
+    });
+  };
 
   render() {
     return (
       <div>
         <h2>Parent Component</h2>
-        {/* <button onClick={this.changeMsgInParent}>Change Message</button> */}
+        <button onClick={this.changeMsgInParent}>Change Message</button>
 
         <hr />
         {/* child comp claaed from Parent Comp */}
         <ChildComp newMessage={this.state.message} />
+        {this.state.showPicture && (
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM7bctX1yQ39f1tL7rK21_O620LywBs0bdU4fBg1P_2qxtEOF4Zxd34UAqPrCJIfMfKj4&usqp=CAU" />
+        )}
       </div>
     );
   }
