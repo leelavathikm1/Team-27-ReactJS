6ca7dc2d935f3cf662988cd829fe6358b3877db0
leelavathikm1 <leelavathikm1@gmail.com>
2025-08-29 15:44:21 +0530

    CRUD

--- REACT-JS/app3-Crud/src/App.jsx
+++ REACT-JS/app3-Crud/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import UserTable from "./Components/UserTable";
+import UserCrud from "./Components/UserCrud";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <UserTable />
+      {/* <UserTable /> */}
+      <UserCrud />
     </div>
   );
 }
