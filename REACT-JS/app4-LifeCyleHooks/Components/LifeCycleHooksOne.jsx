f2241cfd6e0e1b10e517cb1a041dd6704da6e171
leelavathikm1 <leelavathikm1@gmail.com>
2025-09-15 17:02:46 +0530

    lifecycle

---
+++ REACT-JS/app4-LifeCyleHooks/Components/LifeCycleHooksOne.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class LifeCycleHooksOne extends Component {
+  constructor() {
+    super();
+    console.log("welcome To Constructor");
+    this.state = {
+      message: "",
+    };
+  }
+
+  static getDerivedStateFromProps() {
+    console.log("Iam From getDerivedStateFromProps");
+  }
+  render() {
+    console.log("Iam From Render Method");
+    return <div>LifeCycleHooksOne</div>;
+  }
+  componentDidMount() {
+    console.log("Iam form componentDidMount");
+  }
+}
---
+++ REACT-JS/app4-LifeCyleHooks/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LifeCycleHooksOne from "../Components/LifeCycleHooksOne";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <LifeCycleHooksOne />
+    </>
+  );
+}
+
+export default App;
