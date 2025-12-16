e6e90d7853586cef7193c1607a150687460a5aab
leelavathikm1 <leelavathikm1@gmail.com>
2025-12-16 11:44:27 +0530

    localserverAxios

--- REACT-JS/app11-serverCommunication/src/Components/User.jsx
+++ REACT-JS/app11-serverCommunication/src/Components/User.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function User(params) {
   const [userInfo, setUserInfo] = useState({ fname: "", lname: "", email: "" });
+  const [allUserInfo, setAllUserInfo] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+
+  useEffect(() => {
+    getDataFromServer();
+  }, []);
 
   const handleChange = (e) => {
     const newUserInfo = { ...userInfo }; //cloning or copying of userInfo
@@ -10,19 +17,63 @@ export function User(params) {
     console.log(newUserInfo);
   };
 
+  // const createUser = () => {
+  //   fetch("http://localhost:3000/users", {
+  //     method: "POST",
+  //     body: JSON.stringify(userInfo),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then(() => {
+  //       console.log("User Created");
+  //     });
+  // };
+
   const createUser = () => {
-    fetch("http://localhost:3000/users", {
-      method: "POST",
-      body: JSON.stringify(userInfo),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => {
-        return res.json();
-      })
-      .then(() => {
-        console.log("User Created");
+    axios.post("http://localhost:3000/users", userInfo).then(function (res) {
+      console.log("User Created Successfully");
+      getDataFromServer();
+      clearForm();
+    });
+  };
+
+  const getDataFromServer = () => {
+    axios.get("http://localhost:3000/users").then((resp) => {
+      console.log(resp.data);
+      setAllUserInfo(resp.data);
+    });
+  };
+
+  const clearForm = () => {
+    setUserInfo({
+      fname: "",
+      lname: "",
+      email: "",
+    });
+  };
+
+  const handleDelete = (user) => {
+    axios.delete("http://localhost:3000/users/" + user.id).then(() => {
+      getDataFromServer();
+    });
+  };
+
+  const handleEdit = (user) => {
+    setUserInfo(user);
+    setIsEdit(true);
+  };
+
+  const updateUser = () => {
+    axios
+      .put("http://localhost:3000/users/" + userInfo.id, userInfo)
+      .then(function () {
+        clearForm();
+        setIsEdit(false);
+        getDataFromServer();
       });
   };
 
@@ -55,10 +106,60 @@ export function User(params) {
           onChange={handleChange}
         />{" "}
         <br />
-        <button type="button" onClick={createUser}>
+        {isEdit ? (
+          <button type="button" onClick={updateUser}>
+            Update User
+          </button>
+        ) : (
+          <button type="button" onClick={createUser}>
+            Add User
+          </button>
+        )}
+        {/* <button type="button" onClick={createUser}>
           Add User
-        </button>
+        </button> */}
       </form>
+      <br />
+      <table border={1}>
+        <thead>
+          <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Edit</td>
+            <td>Delete</td>
+          </tr>
+        </thead>
+        <tbody>
+          {allUserInfo.map((user) => {
+            return (
+              <tr>
+                <td>{user.fname}</td>
+                <td>{user.lname}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      handleEdit(user);
+                    }}
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      handleDelete(user);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
